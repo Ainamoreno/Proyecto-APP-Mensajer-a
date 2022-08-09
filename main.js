300757dbc11ced738b2bfc526dc025cbfcca9393
Aina <ainamoreno@outlook.com>
2022-08-09 18:42:06 +0200

    pantalla chat + mensajes

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-cargarPantallaCanal = () => {
-    
+const contenidoMensaje = document.getElementById('contenido-mensaje');
+const bloquesMensajes = document.getElementById('bloques-mensajes');
+
+salidaMensaje = () => {
+    let mensaje = contenidoMensaje.value;
+    let estructura = '';
+    let date = new Date();
+    let hora = `${date.getHours()}:${date.getMinutes()}`;
+    estructura +=
+        `<div class="bloque-mensaje-mio">
+                <label class="nombre-mensaje">Yo:</label>
+                <br>
+                <span class="mensaje">${mensaje }<time datetime="${ hora}" class="fecha">${ hora} </time></span>
+        </div>`
+
+    bloquesMensajes.innerHTML += estructura;
+    contenidoMensaje.value = '';
 }
+
